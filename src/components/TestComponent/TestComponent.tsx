c87d5366ec3af1850a12c9fa9d186535e103211e
ricferal <ricferal@gmail.com>
2020-11-14 18:04:57 -0300

    hook

--- src/components/TestComponent/TestComponent.tsx
+++ src/components/TestComponent/TestComponent.tsx
@@ -1,10 +1,19 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import './TestComponent.css'
 
 function TestComponent(props:{name: string}){
   const [age,setAge] =useState(42)
 
-  
+  useEffect(()=> {
+     console.log('Component was created')
+
+  },[])
+
+  useEffect(()=> {
+    console.log('Age has been update to: '+ age)
+
+ },[age])
+
 
   return  <div className="TestComponent">
       Olá, {props.name},{age}
