0001681d64b68443453499f525753ddc5482a836
ricferal <ricferal@gmail.com>
2020-11-14 22:01:48 -0300

    estrutua tabela

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,6 +4,7 @@ import Header from '../Header';
 import Button from '../../shared/Button';
 import Container from '../../shared/Container';
 import Input from '../../shared/Input';
+import Table from '../../shared/Table';
 
 function TestComponent (){
    return <img width="16" src="https://img.icons8.com/pastel-glyph/2x/search--v1.png" alt="search icon"   />
@@ -19,7 +20,7 @@ function App() {
       <Header title="AlgaStock"/>
        
        <Container>
-         
+         <Table/>
 
        </Container>
  
--- src/shared/Table/Table.tsx
+++ src/shared/Table/Table.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
-import './Table.scss'
+//import './Table.scss'
 
 const Table = () =>{
     return <table>
+        <thead>
+             <tr>
+                 <th>Product</th>
+                 <th>Price</th>
+                 <th>Stock</th>
+             </tr>
+        </thead>
 
-        
+        <tbody>
+          <tr>
+             <td>caneta</td>
+             <td>R$ 2,00</td>
+             <td>10 unidades</td>
+          </tr>
+
+       
+        </tbody>
+
     </table>
 } 
 
